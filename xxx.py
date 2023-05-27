a56af04116d595209aec9459469fd2ae6230e225
korayerbas <115890098+korayerbas@users.noreply.github.com>
2023-05-27 20:30:51 +0300

    xxx.py

---
+++ xxx.py
@@ -0,0 +1,34 @@
+from model_csa import CSANET
+from load_data import LoadData, LoadVisualData
+
+from torch.utils.data import DataLoader
+import torch 
+import imageio
+import numpy as np
+from torchvision import transforms
+
+device = torch.device("cuda")
+dslr_scale = float(1) / (2 ** (0 - 1))
+dataset_dir='/content/gdrive/MyDrive/ColabNotebooks/pynet_fullres/dataset/test/full_resolution'
+
+visual_dataset = LoadVisualData(dataset_dir, 3, dslr_scale)
+to_image = transforms.Compose([transforms.ToPILImage()])
+visual_loader = DataLoader(dataset=visual_dataset, batch_size=1, shuffle=False, num_workers=0,
+                               pin_memory=True, drop_last=False)
+
+generator = CSANET(instance_norm=True, instance_norm_level_1=True).to(device)
+generator.load_state_dict(torch.load("/content/gdrive/MyDrive/ColabNotebooks/CSANet/model_csa/86.pth"), strict=False)
+
+generator.eval()
+with torch.no_grad():
+
+  visual_iter = iter(visual_loader)
+  for j in range(len(visual_loader)):
+
+    torch.cuda.empty_cache()
+    raw_image = next(visual_iter)
+    raw_image = raw_image.to(device, non_blocking=True)
+
+    enhanced = generator(raw_image.detach())
+    enhanced = np.asarray(to_image(torch.squeeze(enhanced.detach().cpu())))
+    imageio.imwrite("/content/gdrive/MyDrive/ColabNotebooks/CSANet/results_csa/" + str(j) + ".xxxjpg", enhanced)
